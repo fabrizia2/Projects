68ef95c0dece875e9a2937cfd8de890c8851206e
fabrizia2 <melvinrenish56@gmail.com>
2024-02-28 01:03:01 +0300

    New changes

--- Irvine/script.js
+++ Irvine/script.js
@@ -11,3 +11,20 @@ function stickyHeader() {
     header.classList.remove("sticky");
   }
 }
+
+
+// Initialize variables
+let currentSlide = 0;
+const allSlides = document.querySelectorAll('.slider .slide');
+
+// Show next slide function
+function showNextSlide() {
+    allSlides.forEach(slide => {
+        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
+    });
+    currentSlide = (currentSlide + 1) % (allSlides.length / 4); // Adjust for multiple sections
+}
+
+// Automatically move to the next slide every 3 seconds
+setInterval(showNextSlide, 3000);
+
